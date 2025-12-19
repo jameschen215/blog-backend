f4a01546147694902fbbc70857f45547e33b93ae
James <chenjames215@gmail.com>
2025-12-19 13:50:45 +0800

    feat: add guestName to Comment model

---
+++ prisma/migrations/20251219054915_add/migration.sql
@@ -0,0 +1,9 @@
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "guest_name" TEXT,
+ALTER COLUMN "authorId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
